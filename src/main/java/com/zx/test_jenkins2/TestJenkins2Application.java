dbc6048c7edddc8e5a284e346c97a02b2cfafed6
xiao.zhang <xiao.zhang@mobvista.com>
2023-08-08 18:27:59 +0800

    初始化提交

---
+++ src/main/java/com/zx/test_jenkins2/TestJenkins2Application.java
@@ -0,0 +1,13 @@
+package com.zx.test_jenkins2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJenkins2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJenkins2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/zx/test_jenkins2/TestJenkins2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zx.test_jenkins2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJenkins2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
